b47f4964c7e1e9732bb04601452a88ee08059f30
suganth14 <69384507+suganth14@users.noreply.github.com>
2023-10-11 18:19:10 +0530

    Update cbse.h

--- cbse.h
+++ cbse.h
@@ -1,11 +1,12 @@
 #include "school.h"
+# include <string>
 class cbse:public school
 {
     float student_fees;
     int max_std;
     int no_passed;
-    char uniform_pattern[15];
-    char medium[15];
+    std::string uniform_pattern;
+    std:string medium;
 public:
     void setcbse();
     void getcbse();
